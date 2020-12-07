a091397ba83b4732cda5c23f0b41d41fa1797f6b
Rocco <roccomaniscalco2001@gmail.com>
2020-12-06 21:35:45 -0500

    fixed connection bug

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 let connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = process.env.JAWSDB_URL;
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
     host: "localhost",
@@ -13,7 +13,7 @@ if (process.env.JAWSDB_URL) {
   });
 }
 
-connection.connect(function (err) {
+connection.connect((err) => {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
